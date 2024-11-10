7308cb64a859cf6584bbd7016edbbad4bb26d4aa
osvaldoikuta <osvaldo.ikuta@gazin.com.br>
2024-11-10 18:58:53 -0300

    Created Screen component

---
+++ src/components/Screen/Screen.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useAppSafeArea } from '../../hooks/useAppSafeArea';
+import { useAppTheme } from '../../hooks/useAppTheme';
+import {Box} from '../Box/Box';
+import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
+import { Text } from '../Text/Text';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Icon } from '../Icon/Icon';
+ 
+interface ScreenProps {
+  children: React.ReactNode;
+  canGoBack?: boolean
+  scrollable?: boolean;
+}
+export function Screen({
+  children,
+  canGoBack = false,
+  scrollable = false,
+}: ScreenProps) {
+  const {bottom, top} = useAppSafeArea();
+  const {colors} = useAppTheme();
+
+  const Container = scrollable ? ScrollViewContainer : ViewContainer;
+  return (
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Container backgroundColor={colors.background}>
+        <Box
+          paddingBottom="s24"
+          paddingHorizontal="s24"
+          style={{paddingTop: top, paddingBottom: bottom}}>
+          {canGoBack && (
+            <Box mb="s24" flexDirection="row">
+              <Icon name="arrowLeft" color="primary" />
+              <Text preset="paragraphMedium" semiBold ml="s8">
+                Voltar
+              </Text>
+            </Box>
+          )}
+          {children}
+        </Box>
+      </Container>
+    </KeyboardAvoidingView>
+  );
+}
---
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {Icon} from '../../../components/Icon/Icon';
+import {Button} from '../../../components/Button/Button';
+import { PasswordInput } from '../../../components/TextInput/PasswordInput';
+
+export function LoginScreen() {
+  return (
+    <Screen>
+      <Text marginBottom="s8" preset="headingLarge">
+        Olá
+      </Text>
+      <Text preset="paragraphLarge" mb="s40">
+        Digite seu e-mail e senha para entrar
+      </Text>
+
+      <TextInput
+        errorMessage="mensagem de error"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
+
+      <PasswordInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{mb: 's10'}}
+      />
+
+      <Text color="primary" preset="paragraphSmall" bold>
+        Esqueci minha senha
+      </Text>
+
+      <Button marginTop="s48" title="Entrar" />
+      <Button preset="outline" marginTop="s12" title="Criar uma conta" />
+    </Screen>
+  );
+}
