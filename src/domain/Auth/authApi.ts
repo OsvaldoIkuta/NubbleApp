107f13045a3ae7979b4f57e8334c0f34a1ea7626
osvaldoikuta <osvaldo.ikuta@gazin.com.br>
2024-12-08 10:21:55 -0300

    authorization with JWT and refresh token

--- src/domain/Auth/authApi.ts
+++ src/domain/Auth/authApi.ts
@@ -1,10 +1,14 @@
 import {api} from '@api';
 import { AuthCredentialsAPI, FieldIsAvailableAPI, ForgotPasswordParam, SignUpDataAPI, UserAPI } from '@domain';
+import { AxiosRequestConfig } from 'axios';
+
+const REFRESH_TOKEN_URL = 'auth/refresh-token';
+
 async function signIn(
   email: string,
   password: string,
 ): Promise<AuthCredentialsAPI> {
-  const response = await api.post<AuthCredentialsAPI>('login', {
+  const response = await api.post<AuthCredentialsAPI>('auth/login', {
     email,
     password,
   });
@@ -14,7 +18,7 @@ async function signIn(
 async function isUserNameAvailable(params: {
   username: string;
 }): Promise<FieldIsAvailableAPI> {
-  const response = await api.get<FieldIsAvailableAPI>('validate-username', {
+  const response = await api.get<FieldIsAvailableAPI>('auth/validate-username', {
     params,
   });
 
@@ -24,7 +28,7 @@ async function isUserNameAvailable(params: {
 async function isEmailAvailable(params: {
   email: string;
 }): Promise<FieldIsAvailableAPI> {
-  const response = await api.get<FieldIsAvailableAPI>('validate-email', {
+  const response = await api.get<FieldIsAvailableAPI>('auth/validate-email', {
     params,
   });
 
@@ -32,21 +36,38 @@ async function isEmailAvailable(params: {
 }
 
 async function signOut(): Promise<string> {
-  const response = await api.get<string>('profile/logout');
+  const response = await api.get<string>('auth/profile/logout');
   return response.data;
 }
 
 async function signUp(data: SignUpDataAPI): Promise<UserAPI> {
-  const response = await api.post<UserAPI>('register', data);
+  const response = await api.post<UserAPI>('auth/register', data);
   return response.data;
 }
 
 async function forgotPassword(
   params: ForgotPasswordParam,
 ): Promise<{message: string}> {
-  const response = await api.post<{message: string}>('forgot-password', params);
+  const response = await api.post<{message: string}>('auth/forgot-password', params);
 
   return response.data;
+}
+
+async function refreshToken(token: string): Promise<AuthCredentialsAPI> {
+  const response = await api.post<AuthCredentialsAPI>(REFRESH_TOKEN_URL, {
+    refreshToken: token,
+  });
+
+  return response.data;
+}
+
+/**
+ * @param axiosConfig [AxiosRequestConfig](https://axios-http.com/docs/req_config) - The Axios request configuration
+ * @returns  Check the config URL property to returns if is a refresh token request
+ */
+function isRefreshTokenRequest(request: AxiosRequestConfig): boolean {
+  const url = request.url;
+  return url === REFRESH_TOKEN_URL;
 }
 
 export const authApi = {
@@ -56,4 +77,6 @@ export const authApi = {
   isUserNameAvailable,
   isEmailAvailable,
   forgotPassword,
+  refreshToken,
+  isRefreshTokenRequest,
 };
--- src/domain/Auth/authTypes.ts
+++ src/domain/Auth/authTypes.ts
@@ -1,6 +1,8 @@
 import {User, UserAPI} from '../User';
 export interface AuthCredentials {
   token: string;
+  tokenExpiresAt: string;
+  refreshToken: string;
   user: User;
 }
 
@@ -13,6 +15,8 @@ export interface AuthCredentialsAPI {
   auth: {
     type: string; //'bearer';
     token: string; // 'NA.GCfDf81QRs0q4VxyFSEvWs8kZ-DoZnl5zKLn8UDY8ntedjZCPgxVxfFijlQy';
+    refreshToken: string;
+    expires_at: string; // '2023-10-01T09:12:59.043+00:00';
   };
   user: UserAPI;
 }
