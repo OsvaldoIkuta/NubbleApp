1de1226004e76127a83afdd275f100702e6afc9f
osvaldoikuta <osvaldo.ikuta@gazin.com.br>
2025-02-01 15:27:19 -0300

    25 - Dark Mode(Zustand, MMKV and Components with Typescript Generics)

--- src/assets/icons/MessageIcon.tsx
+++ src/assets/icons/MessageIcon.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {Svg, Path} from 'react-native-svg';
 
 import {IconBase} from '../../components/Icon/Icon';
+
 export function MessageIcon({size = 20, color = 'black'}: IconBase) {
   return (
     <Svg width={size} height={size} viewBox="0 0 20 20" fill="none">
